44f2fd5cfe66174a630cfcc16a9383a26e1a6403
Odell Isaac <odellisaac@Odells-MacBook-Pro.local>
2024-09-26 17:09:34 -0500

    added modal functionality to generator

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,13 +1,67 @@
+import { useState } from "react";
+import { WORKOUTS } from "../utilis/soldiers";
 import SectionWrapper from "./SectionWrapper";
+
+function Header(props) {
+  const { index, title, description } = props;
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
+          {index}
+        </p>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
+      </div>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
+    </div>
+  );
+}
 
 export default function Generator() {
   // const { children } = props;
+
+  const [showModal, setShowModal] = useState(false);
+  let toggleModal = () => {
+    setShowModal(!showModal);
+  };
   return (
     <SectionWrapper
       header={"generate your workout"}
       title={["It's", "Huge", " o'clock"]}
     >
-      asdfe
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to endure"}
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+              className="bg-slate-950 border border-blue-400 py-3 rounded duration-200 hover:border-blue-600"
+              key={typeIndex}
+            >
+              <p className="capitalize">{type.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
+      </div>
+      <Header
+        index={"02"}
+        title={"Lock on targets"}
+        description={"Select the muscles judged for annhilation"}
+      />
+      <div className="bg-slate-950 border border-solid border-blue-400 rounded-lg flex flex-col">
+        <button
+          className="relative flex items-center justify-center p-3 "
+          onClick={toggleModal}
+        >
+          <p>Select muscle groups</p>
+          <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
+        </button>
+        {showModal && <div>modal</div>}
+      </div>
     </SectionWrapper>
   );
 }
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -2,7 +2,7 @@ export default function SectionWrapper(props) {
   const { children, header, title } = props;
   return (
     <section className="min-h-screen flex flex-col gap-10">
-      <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
+      <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center p-4">
         <p className="uppercase font-medium">{header}</p>
         <h2 className="font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
           {title[0]}
@@ -10,7 +10,9 @@ export default function SectionWrapper(props) {
           {title[2]}
         </h2>
       </div>
-      {children}
+      <div className="max-w-[800px] w-full flex flex-col mx-auto gap-10 p-4">
+        {children}
+      </div>
     </section>
   );
 }
