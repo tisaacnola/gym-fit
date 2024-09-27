b35a1c540b2849ab7d5a7f0f6219fc80e51c1d79
tisaacnola <oisaac47@gmail.com>
2024-09-27 13:57:22 -0500

    added increment button to cards

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ function App() {
       return;
     }
     let newWorkout = generateWorkout({ poison, muscles, goal });
-    console.log(newWorkout);
-    // setWorkout(newWorkout);
+    setWorkout(newWorkout);
   }
 
   return (
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,5 +1,16 @@
+import ExerciseCard from "./ExerciseCard";
+import SectionWrapper from "./SectionWrapper";
+
 export default function Workout(props) {
   const { workout } = props;
 
-  return <div>{workout}</div>;
+  return (
+    <SectionWrapper header={"welcome to "} title={["The ", "Danger", " Zone"]}>
+      <div className="flex flex-col gap-4">
+        {workout.map((exercise, index) => {
+          return <ExerciseCard exercise={exercise} index={index} key={index} />;
+        })}
+      </div>
+    </SectionWrapper>
+  );
 }
