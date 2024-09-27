19cf052f7c012bf7d4a168e0ac86d1a9aa925b8d
tisaacnola <oisaac47@gmail.com>
2024-09-26 19:36:32 -0500

    added functionality to modal

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -28,6 +28,29 @@ export default function Generator() {
   let toggleModal = () => {
     setShowModal(!showModal);
   };
+
+  function updateMuscles(muscleGroup) {
+    if (muscles.includes(muscleGroup)) {
+      setMuscles(muscles.filter((val) => val !== muscleGroup));
+      return;
+    }
+
+    if (muscles.length > 2) {
+      return;
+    }
+
+    if (poison !== "individual") {
+      setMuscles([muscleGroup]);
+      setShowModal(false);
+      return;
+    }
+
+    setMuscles([...muscles, muscleGroup]);
+    if (muscles.length === 2) {
+      setShowModal(false);
+    }
+  }
+
   return (
     <SectionWrapper
       header={"generate your workout"}
@@ -43,6 +66,7 @@ export default function Generator() {
           return (
             <button
               onClick={() => {
+                setMuscles([]);
                 setPoison(type);
               }}
               className={
@@ -66,10 +90,36 @@ export default function Generator() {
           className="relative flex items-center justify-center p-3 "
           onClick={toggleModal}
         >
-          <p>Select muscle groups</p>
+          <p className="capitalize">
+            {muscles.length == 0 ? "Select muscle groups" : muscles.join(" ")}
+          </p>
           <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
         </button>
-        {showModal && <div>modal</div>}
+        {showModal && (
+          <div className=" flex flex-col px-3 pb-3">
+            {(poison === "individual"
+              ? WORKOUTS[poison]
+              : Object.keys(WORKOUTS[poison])
+            ).map((muscleGroup, muscleGroupIndex) => {
+              return (
+                <button
+                  onClick={() => {
+                    updateMuscles(muscleGroup);
+                  }}
+                  key={muscleGroupIndex}
+                  className={
+                    "hover:text-blue-400 duration-200 " +
+                    (muscles.includes(muscleGroup) ? " text-blue-400" : " ")
+                  }
+                >
+                  <p className="uppercase">
+                    {muscleGroup.replaceAll("_", " ")}
+                  </p>
+                </button>
+              );
+            })}
+          </div>
+        )}
       </div>
       <Header
         index={"03"}
